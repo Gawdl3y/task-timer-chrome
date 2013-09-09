4185c50cf1748811a4fe8676dfac4576913f5dcc
Gawdl3y <Gawdl3y@gmail.com>
2013-09-09 17:42:26 -0400

    Added check for the app already being open to the background alarm event

--- scripts/background.js
+++ scripts/background.js
@@ -5,21 +5,21 @@ function onMessage(message, sender, sendResponse) {
 
 // An alarm went off!
 function onAlarm(alarm) {
-	if(localStorage['background-running'] == 'true') window.open('main.html');
+	if(localStorage['background-running'] === 'true' && localStorage['opened'] !== 'true') window.open('main.html');
 }
 
 // The app was installed/updated, or Chrome was updated
 function onUpdate(details) {
-	if(details.reason == "update") {
+	if(details.reason === 'update') {
 		// Open the app if it isn't already open, we're on a new version, and update alerting is enabled
-		if(typeof localStorage['opened'] == 'undefined' || localStorage['opened'] == 'false') {
-			if(typeof localStorage['old-version'] != 'undefined' &&  localStorage['old-version'] != chrome.app.getDetails().version) {
-				if(typeof localStorage['update-alert'] == 'undefined' || localStorage['update-alert'] == 'true') {
+		if(typeof localStorage['opened'] === 'undefined' || localStorage['opened'] === 'false') {
+			if(typeof localStorage['old-version'] !== 'undefined' &&  localStorage['old-version'] !== chrome.app.getDetails().version) {
+				if(typeof localStorage['update-alert'] === 'undefined' || localStorage['update-alert'] === 'true') {
 					window.open('main.html');
 				}
 			}
 		}
-	} else if(details.reason == "install") {
+	} else if(details.reason === 'install') {
 		// Open the app installed page
 		window.open('installed.html');
 	}
